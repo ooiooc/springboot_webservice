8502b1ba469f3a76dfd28da77bd1d0405205525f
ooiooc <shinjh1102@naver.com>
2022-01-04 02:17:44 +0900

    test 생성

---
+++ src/test/java/com/isle/project/springboot/web/HelloResponseDtoTest.java
@@ -0,0 +1,24 @@
+package com.isle.project.springboot.web;
+
+import com.isle.project.springboot.web.dto.HelloResponseDto;
+import org.junit.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class HelloResponseDtoTest {
+
+    @Test
+    public void 롬복_기능테스트() {
+        //given
+        String name = "test";
+        int amount = 1000;
+
+        //when
+        HelloResponseDto dto = new HelloResponseDto(name,amount);
+
+        //then
+        assertThat(dto.getName()).isEqualTo(name);
+        assertThat(dto.getAmount()).isEqualTo(amount);
+
+    }
+
+}
